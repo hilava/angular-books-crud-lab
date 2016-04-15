6a1fa79e1dfaa833ebb1c1be8ce5524d42ea5eef
Hila Vasiler <hila.marmel@gmail.com>
2016-04-15 14:57:15 -0700

    create book, shoe and index

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -2,6 +2,46 @@ angular.module('libraryApp')
   .controller('BooksIndexController', BooksIndexController);
 
 BooksIndexController.$inject=['$http'];
+
 function BooksIndexController( $http ) {
   var vm = this;
+  var endpoint = "https://super-crud.herokuapp.com/books";
+  vm.newBook="";
+
+  $http({
+    method: 'GET',
+    url: endpoint
+  }).then(indexSuccess, indexError);
+
+
+  function indexSuccess(response){
+    vm.books = response.data.books;
+  }
+
+  function indexError(err){
+    console.log(err);
+  }
+
+vm.createAlbum = function() {
+  $http({
+    method: 'POST',
+    url: endpoint,
+    data: vm.newBook,
+  }).then(createSuccess, createError);
+};
+
+function createSuccess(response){
+  vm.books.push(response.data);
+}
+
+function createError(err){
+  console.log(err);
+}
+
+
+
+
+
+
+//close BooksIndexController
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,35 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+  .controller('BooksShowController', BooksShowController);
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
+
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+  vm.book = {};
+  var endpoint = "https://super-crud.herokuapp.com/books";
+
+$http({
+  method: 'GET',
+  url: endpoint + "/" + $routeParams.id
+  }).then(showSuccess, showError);
+
+
+function showSuccess(response){
+  console.log(response);
+  vm.book = response.data;
+}
+
+function showError(err){
+  console.log(err);
+}
+
+  // $http({
+  // method: 'GET',
+  // url: 'api/albums/' + $routeParams.id
+  // }).then(function success(response) {
+  //   vm.album = response.data;
+  // }, function errorCallback(response) {
+  //     console.log('There was an error getting the data', response);
+  // });
+
+}
